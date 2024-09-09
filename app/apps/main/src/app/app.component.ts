603c96183bfee3d8948bd76e0d253c1a9ccbb703
Vazgen Mikaelian <vazgvazg2@gmail.com>
2024-09-10 01:04:38 +0400

    Тестирование winbox.

--- app/apps/main/src/app/app.component.ts
+++ app/apps/main/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TuiButton, TuiLabel, TuiRoot } from '@taiga-ui/core';
+import { WinboxService } from './services';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TuiRoot, TuiButton, TuiLabel],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected readonly number = this.winboxService.testNumber;
+
+  public constructor(private readonly winboxService: WinboxService) {}
+
+  public onOpen() {
+    this.winboxService.openBox();
+  }
+}
