28e8b260447e2a08101f8db51ce03c6cd8d63294
Vazgen Mikaelian <vazgvazg2@gmail.com>
2024-09-10 21:44:34 +0400

    Добавление объектов в efcore.

--- services/store-apps/src/Rubrum.Platform.StoreAppsService.EntityFrameworkCore/StoreAppsServiceDbContextModelCreatingExtensions.cs
+++ services/store-apps/src/Rubrum.Platform.StoreAppsService.EntityFrameworkCore/StoreAppsServiceDbContextModelCreatingExtensions.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Rubrum.Platform.StoreAppsService.Apps;
 using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using static Rubrum.Platform.StoreAppsService.StoreAppsServiceDbProperties;
 
 namespace Rubrum.Platform.StoreAppsService.EntityFrameworkCore;
 
@@ -9,5 +12,15 @@ public static class StoreAppsServiceDbContextModelCreatingExtensions
         this ModelBuilder builder)
     {
         Check.NotNull(builder, nameof(builder));
+
+        builder.Entity<App>(b =>
+        {
+            b.ToTable(DbTablePrefix + "Apps", DbSchema);
+
+            b.ConfigureByConvention();
+
+            b.Property(x => x.Version).IsRequired();
+            b.Property(x => x.Name).IsRequired();
+        });
     }
 }
