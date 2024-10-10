ac722f4f952dbe3deb5e0e68758729b07d0dc018
Artem Sharypov <79115666058@ya.ru>
2024-10-10 10:53:44 +0300

    Добавил  в авторизацию получение permission через кэш и создание через  эвентыэвенты

---
+++ framework/src/Rubrum.Authorization/Permissions/GiveGrantPermissionEto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.EventBus;
+
+namespace Rubrum.Authorization.Permissions;
+
+[Serializable]
+[EventName("Rubrum.Authorization.Permissions.GiveGrantPermission")]
+public sealed record GiveGrantPermissionEto(
+    string PermissionName,
+    string ProviderName,
+    string ProviderKey,
+    bool IsGranted);
---
+++ framework/src/Rubrum.Authorization/Permissions/PermissionCacheItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Rubrum.Authorization.Permissions;
+
+public class PermissionCacheItem
+{
+    public const string CacheKeyFormat = "permission:{0}";
+
+    public bool IsEnable { get; set; }
+
+    public static string CalculateCacheKey(string name)
+    {
+        return string.Format(CacheKeyFormat, name);
+    }
+}
